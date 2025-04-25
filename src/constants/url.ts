42814aeb4d2107dafe237982a79467ca7c1b5b7f
Franco Carballar <francocarballar@gmail.com>
2025-04-25 02:49:09 -0300

    feat: :sparkles: añade constantes de URL para el repositorio y plantillas
    - Se ha creado un nuevo archivo `url.ts` en la carpeta `src/constants` que define las constantes `REPOSITORY_BASE_URL` y `TEMPLATES_BASE_URL`.
    - Estas constantes proporcionan una forma centralizada de gestionar las URLs utilizadas en la aplicación, mejorando la mantenibilidad y la claridad del código.
    
    Este cambio establece una base sólida para la gestión de URLs en el proyecto, facilitando futuras modificaciones y reduciendo la posibilidad de errores en las rutas.

---
+++ src/constants/url.ts
@@ -0,0 +1,11 @@
+/**
+ * @constant {string} REPOSITORY_BASE_URL
+ * @description La URL base del repositorio.
+ */
+export const REPOSITORY_BASE_URL = 'https://api.sherry.social/v1/repository/v1'
+
+/**
+ * @constant {string} TEMPLATES_BASE_URL
+ * @description La URL base de las plantillas.
+ */
+export const TEMPLATES_BASE_URL = 'https://api.sherry.social/templates/v1'
