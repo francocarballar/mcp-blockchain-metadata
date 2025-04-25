8b844ca8e9cf8523d19e2ad90f46b860a884070c
Franco Carballar <francocarballar@gmail.com>
2025-04-25 02:53:42 -0300

    feat: :sparkles: añade nuevas constantes de URL para listas de tokens de Trader Joe
    - Se han añadido las constantes `LFJ_VERIFIED_TOKENLIST_URL` y `LFJ_POPULAR_TOKENLIST_URL`, que proporcionan acceso a los listados de tokens verificados y populares de Trader Joe en GitHub.
    - Se ha creado la constante `PROTOCOL_TOKENLIST_URLS`, que mapea protocolos a sus URLs de tokenlists oficiales, mejorando la organización y accesibilidad de estas URLs en el código.
    
    Estos cambios amplían la gestión de URLs en el proyecto, facilitando futuras modificaciones y mejorando la claridad y mantenibilidad del código.

--- src/constants/url.ts
+++ src/constants/url.ts
@@ -1,11 +1,36 @@
 /**
  * @constant {string} REPOSITORY_BASE_URL
- * @description La URL base del repositorio.
+ * @description URL base para acceder a la API del repositorio de metadatos blockchain.
  */
 export const REPOSITORY_BASE_URL = 'https://api.sherry.social/v1/repository/v1'
 
 /**
  * @constant {string} TEMPLATES_BASE_URL
- * @description La URL base de las plantillas.
+ * @description URL base para acceder a las plantillas de mini aplicaciones.
  */
 export const TEMPLATES_BASE_URL = 'https://api.sherry.social/templates/v1'
+
+/**
+ * @constant {string} LFJ_VERIFIED_TOKENLIST_URL
+ * @description URL al listado de tokens verificados de Trader Joe en GitHub.
+ */
+export const LFJ_VERIFIED_TOKENLIST_URL =
+  'https://github.com/traderjoe-xyz/joe-tokenlists-v2/blob/main/verified_tokenlist.json'
+
+/**
+ * @constant {string} LFJ_POPULAR_TOKENLIST_URL
+ * @description URL al listado de tokens populares de Trader Joe en GitHub.
+ */
+export const LFJ_POPULAR_TOKENLIST_URL =
+  'https://github.com/traderjoe-xyz/joe-tokenlists-v2/blob/main/popular_tokenlist.json'
+
+/**
+ * @constant {Record<string, string>} PROTOCOL_TOKENLIST_URLS
+ * @description Mapeo de protocolos a sus URLs de tokenlists oficiales.
+ * Actualmente incluye los listados para LiquidityForJoe (lfj) y TraderJoe.
+ */
+export const PROTOCOL_TOKENLIST_URLS: Record<string, string> = {
+  lfj: 'https://raw.githubusercontent.com/traderjoe-xyz/joe-tokenlists-v2/main/verified_tokenlist.json',
+  traderjoe:
+    'https://raw.githubusercontent.com/traderjoe-xyz/joe-tokenlists-v2/main/verified_tokenlist.json'
+}
